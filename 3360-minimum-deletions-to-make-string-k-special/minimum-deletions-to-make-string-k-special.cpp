4ac48c57d29ab3aaecefc23c5511b81d53b9d654
santhiyal22 <153731991+santhiyal22@users.noreply.github.com>
2025-06-21 00:06:44 -0700

    Time: 17 ms (34.42%) | Memory: 19.6 MB (50.00%) - LeetSync

---
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumDeletions(string word, int k) {
+        int n = word.size();
+        unordered_map<char, int> freq;
+        for(char c: word) {
+            freq[c]++;
+        }
+
+        int ans = n;
+        for(auto [minchar, f1]:freq) {
+            int del = 0;
+            for(auto [currChar, f2]: freq) {
+                int diff = f2-f1;
+                if(f1>f2) {
+                    del += f2;
+                } else if(diff >= k) {
+              del += (f2-f1-k);
+                }
+            }
+            ans = min(ans, del);
+        }
+        return ans;
+    }
+};
